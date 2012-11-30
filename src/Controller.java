633dd5ff1e89c45f15dde669d65a48e1e465de24
Mudrekh <mgoderya@hotmail.com>
2012-11-30 00:35:50 -0600

    Fixed issure with variable names in controller and UI

--- src/Controller.java
+++ src/Controller.java
@@ -60,13 +60,13 @@ public class Controller
               }
           }
       } catch (ClassNotFoundException ex) {
-          java.util.logging.Logger.getLogger(UserInterfacePrototype.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+          java.util.logging.Logger.getLogger(UserInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
       } catch (InstantiationException ex) {
-          java.util.logging.Logger.getLogger(UserInterfacePrototype.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+          java.util.logging.Logger.getLogger(UserInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
       } catch (IllegalAccessException ex) {
-          java.util.logging.Logger.getLogger(UserInterfacePrototype.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+          java.util.logging.Logger.getLogger(UserInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
       } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-          java.util.logging.Logger.getLogger(UserInterfacePrototype.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+          java.util.logging.Logger.getLogger(UserInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
       }
       //</editor-fold>
 
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -219,20 +219,20 @@ public class UserInterface extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(UserInterfacePrototype.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UserInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(UserInterfacePrototype.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UserInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(UserInterfacePrototype.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UserInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(UserInterfacePrototype.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(UserInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new UserInterfacePrototype().setVisible(true);
+                new UserInterface().setVisible(true);
             }
         });
     }
